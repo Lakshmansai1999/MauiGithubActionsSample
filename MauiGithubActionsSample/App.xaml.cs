e340484b3fb5abef2f5cd67262c8ce29856d1052
Lakshmansai1999 <klakshmansai@stratapps.com>
2023-12-20 17:16:25 +0530

    first commit

---
+++ MauiGithubActionsSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGithubActionsSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiGithubActionsSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiGithubActionsSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
